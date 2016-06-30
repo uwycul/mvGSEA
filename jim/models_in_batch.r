7d9284f508a361ef786511f8d4f00c588f847063
Zhang <zhangz@email.chop.edu>
2016-06-30 15:22:38 -0400

    examples

--- jim/models_in_batch.r
+++ jim/models_in_batch.r
@@ -1,1 +1,25 @@
 # An alternative way to build regression models in a batch
+
+# ys is the matrix of multiple dependant variables
+# xs is the matrix of multiple independant variables
+
+ys.list <- lapply(1:ncol(ys), function(i) ys[, i]); # turn ys into a list;
+
+d <- data.frame(xs, one_y = ys.list[[1]]); # create the data.frame for modeling
+
+# Create the formula
+e <- paste(colnames(xs), collapse=' + '); 
+f <- formula(paste('one_y', e, sep=' ~ '));
+
+result <- parallel::mclapply(ys.list, function(y) { # Run parallel computing
+  
+  d$one_y <- y; # replace the dependent variable with a new one
+  
+  # create model using glm or speedglm
+  model <- glm(f, data=d, family=binomial);
+  
+  # retrieve statistics from the model, such as p value of Wald test
+  c(); # return the statistics
+  
+}, mc.core=4); 
+
